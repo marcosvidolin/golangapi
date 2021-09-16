2ad500ca402f81809acb75a9165cd9350e7c623f
Marcos Vidolin <marcosvidolin@gmail.com>
2021-09-16 17:18:07 -0300

    refact: slice already is a pointer

--- service/question.go
+++ service/question.go
@@ -11,8 +11,8 @@ type Service interface {
 	UpdateQuestion(ctx context.Context, question *domain.Question) (*domain.Question, error)
 	DeleteQuestion(ctx context.Context, id string) error
 	FindQuestionById(ctx context.Context, id string) (*domain.Question, error)
-	FindAllQuestions(ctx context.Context) (*[]domain.Question, error)
-	FindQuestionsByAuthor(ctx context.Context, username string) (*[]domain.Question, error)
+	FindAllQuestions(ctx context.Context) ([]domain.Question, error)
+	FindQuestionsByAuthor(ctx context.Context, username string) ([]domain.Question, error)
 	CreateAnswer(ctx context.Context, questionId string, answer *domain.Answer) (*domain.Question, error)
 	UpdateAnswer(ctx context.Context, questionId string, answer *domain.Answer) (*domain.Question, error)
 }
